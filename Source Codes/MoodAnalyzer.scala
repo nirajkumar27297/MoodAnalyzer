3da383cb9832023a4fa3e3e052036c69003fccb0
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-08-25 20:22:10 +0530

    Niraj[Refracted]:Scala source codes and scala test codes for UserCase 3

--- Source Codes/MoodAnalyzer.scala
+++ Source Codes/MoodAnalyzer.scala
@@ -1,18 +1,6 @@
 package MoodAnalyzerProject
 
-class MoodAnalysisException(msg:String) extends Exception(msg){}
-
-class checkMoodException {
-  @throws(classOf[MoodAnalysisException])
-  def validateMood(msg:String): Unit = {
-    if (msg == null) {
-      throw new MoodAnalysisException("HAPPY")
-    }
-    else if(msg == "") {
-      throw new MoodAnalysisException("EMPTY STRING")
-    }
-  }
-}
+class MoodAnalysisException(msg:CustomException.Value) extends Exception(msg.toString){}
 
 class MoodAnalyzer(message:String) {
   val this.message:String = message
@@ -21,19 +9,20 @@ class MoodAnalyzer(message:String) {
     this("SAD")
   }
   def analyzeMood():String = {
-    val checkMood = new checkMoodException()
     try {
-      checkMood.validateMood(this.message)
-      if (this.message.contains("SAD")) {
+      if(this.message.length == 0 ){
+        throw new MoodAnalysisException(CustomException.emptyString)
+      }
+      else if (this.message.contains("SAD")) {
         return "SAD"
       }
-
       else {
         return "HAPPY"
       }
     }
     catch {
-      case ex: Exception => throw ex
+      case ex:NullPointerException => throw new MoodAnalysisException(CustomException.nullString)
     }
+
   }
 }
--- Test Codes/MoodAnalyzerTestCases.scala
+++ Test Codes/MoodAnalyzerTestCases.scala
@@ -1,9 +1,9 @@
 package moodAnalyzerTest
-import MoodAnalyzerProject.MoodAnalyzer
+import MoodAnalyzerProject.{CustomException, MoodAnalysisException, MoodAnalyzer}
 import org.scalatest.FunSuite
 
-class MoodAnalyzerTestCases extends FunSuite {
 
+class MoodAnalyzerTestCases extends FunSuite {
 
   test("MoodAnlyzerTest_InputIs_InSadMood_ReturnSAD") {
     val objMoodAnalyzer = new MoodAnalyzer("I am in SAD mood")
@@ -18,23 +18,20 @@ class MoodAnalyzerTestCases extends FunSuite {
     }
   }
 
-  test("MoodAnalyzerTest_InputIs_Null_ReturnException") {
+  test("MoodAnalyzerTest_InputIsNull_ReturnCustomException") {
     val objMoodAnalyzer = new MoodAnalyzer(null)
-    val thrown = intercept[Exception] {
+    val thrown = intercept[MoodAnalysisException] {
       objMoodAnalyzer.analyzeMood()
     }
-    assert(thrown.getMessage == "HAPPY")
-    }
-
-
-  test("MoodAnalyzerTest_InputIs_Empty_ReturnException") {
-    val objMoodAnalyzer = new MoodAnalyzer("")
-    val thrown = intercept[Exception] {
-      objMoodAnalyzer.analyzeMood()
-    }
-    assert(thrown.getMessage == "EMPTY STRING")
+    assert(thrown.getMessage == CustomException.nullString.toString)
   }
-
+  test("MoodAnalyzerTest_InputIsEmpty_ReturnCustomException") {
+    val objMoodAnalyzer = new MoodAnalyzer("")
+    val thrown = intercept[MoodAnalysisException] {
+      objMoodAnalyzer.analyzeMood()
+    }
+    assert(thrown.getMessage == CustomException.emptyString.toString)
+  }
 }
 
 
