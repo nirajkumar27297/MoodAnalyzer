67b6db114f974d104309ff7ae7cb1f6caea507d1
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-08-25 12:29:16 +0530

    Niraj[Added]:Scala source and test codes for Mood Analyzer UserCase 3

--- Source Codes/MoodAnalyzer.scala
+++ Source Codes/MoodAnalyzer.scala
@@ -1,5 +1,19 @@
 package MoodAnalyzerProject
 
+class MoodAnalysisException(msg:String) extends Exception(msg){}
+
+class checkMoodException {
+  @throws(classOf[MoodAnalysisException])
+  def validateMood(msg:String): Unit = {
+    if (msg == null) {
+      throw new MoodAnalysisException("HAPPY")
+    }
+    else if(msg == "") {
+      throw new MoodAnalysisException("EMPTY STRING")
+    }
+  }
+}
+
 class MoodAnalyzer(message:String) {
   val this.message:String = message
 
@@ -7,11 +21,19 @@ class MoodAnalyzer(message:String) {
     this("SAD")
   }
   def analyzeMood():String = {
-    if(this.message.contains("SAD")){
-      return "SAD"
+    val checkMood = new checkMoodException()
+    try {
+      checkMood.validateMood(this.message)
+      if (this.message.contains("SAD")) {
+        return "SAD"
+      }
+
+      else {
+        return "HAPPY"
+      }
     }
-    else {
-      return "HAPPY"
+    catch {
+      case ex: Exception => throw ex
     }
   }
 }
